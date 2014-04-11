0cf3727e9ed5c6e8c9f72caf5a33f07d7c784e6a
pablodecm <pablodecm@gmail.com>
2014-04-11 23:01:45 +0200

    Added subtitle and octopress based theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Pablo de Castro'
 SITENAME = u'Particular Attention'
+SITESUBTITLE = u'Blogging. Learning. Optimizing. Graduating.'
 SITEURL = ''
 
 TIMEZONE = 'Europe/Paris'
@@ -26,6 +27,8 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
+
+THEME = "/home/pablodecm/Dropbox/Workspace/pelican-octopress-theme"
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
